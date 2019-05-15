d575275bd7673722f9dbd84a82baf467b458f3f1
Elodie Billon <elodie.billon1995@outlook.fr>
2019-05-15 11:17:49 +0200

    Quête Symfony 7 réalisée

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -1,22 +1,34 @@
 <?php
-// src/Controller/BlogController.php
 namespace App\Controller;
 
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Article;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog_index")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'owner' => 'Elodie',
-        ]);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles]
+        );
     }
 
     /**
@@ -26,13 +38,73 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/list.html.twig', ['page' => $page]);
     }
+
     /**
-     * @Route("/blog/show/{slug}", requirements={"slug"="[a-z0-9\-]+"}, name="blog_show")
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/blog/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     * @return Response A response instance
      */
-    public function show($slug = "Article Sans Titre")
+    public function show(?string $slug): Response
     {
-        $slug= str_replace('-',' ', $slug);
-        $slug = ucwords($slug);
-        return $this->render('blog/show.html.twig', ['slug' => $slug]);
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with ' . $slug . ' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug
+            ]
+        );
+    }
+
+    /**
+     * @Route("blog/category/{categoryName}", name="show_category")
+     * defaults={"categoryName" = null},
+     * @return Response A response instance
+     */
+
+    public function showByCategory(string $categoryName) : Response
+    {
+        if (!$categoryName) {
+        throw $this->createNotFoundException('this category doesn\'nt exists.');
+        }
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' => mb_strtolower($categoryName)]);
+
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $category], ['id' => 'DESC'], 3);
+
+        return $this->render(
+            'blog/category.html.twig',
+            [
+                'articles' => $articles,
+                'category' => $category
+            ]
+        );
     }
 }
---
+++ blog/src/Migrations/Version20190514085930.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190514085930 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article DROP FOREIGN KEY category_id');
+        $this->addSql('ALTER TABLE article ADD category_id INT NOT NULL, CHANGE title title VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E6612469DE2');
+        $this->addSql('DROP INDEX IDX_23A0E6612469DE2 ON article');
+        $this->addSql('ALTER TABLE article DROP category_id, CHANGE title title VARCHAR(100) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT category_id FOREIGN KEY (id) REFERENCES category (id)');
+    }
+}
